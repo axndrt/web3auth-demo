46b3f010c7af0cc45e1ae608491fb23a5242f2cd
Alex Burak <buraksasht@gmail.com>
2024-05-09 12:09:27 +0300

    Add many to many comment

--- authors_books/models.py
+++ authors_books/models.py
@@ -1,20 +1,16 @@
 import django.db.models
 from django.db import models
 
-
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
-
     last_name = models.CharField(max_length=30)
-
     birthday = models.DateTimeField()
-
 
 class Book(models.Model):
     name = models.CharField(max_length=50)
-
     description = models.TextField(blank=True, max_length=200)
 
+    # Note: Relations between books and authors may be organized as Many to Many
     author = models.ForeignKey(
         Author, on_delete=django.db.models.PROTECT, related_name="books"
     )
